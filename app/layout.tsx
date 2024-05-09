798babc64eb2aeaf4bf71f562271f6a2adee28dd
JonnXor <jonnxor@gmail.com>
2024-05-09 14:45:03 +0000

    seventh commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
+import "@mantine/form/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/charts/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/code-highlight/styles.css";
+import "@mantine/tiptap/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/carousel/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/modals/styles.css";
+import "@mantine/nprogress/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jón Agnar Stefánsson",
-  description: "CV for Jón Agnar Stefánsson",
+  description: "CV and portfolio of Jón Agnar Stefánsson",
 };
 
 export default function RootLayout({
@@ -16,7 +31,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={inter.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
