3cfa7424b4785441c2ce4fe5f3e5e30d8639849a
JonnXor <jonnxor@gmail.com>
2024-06-13 13:02:54 +0000

    feat: default locale?

--- middleware.ts
+++ middleware.ts
@@ -10,5 +10,5 @@ export default createMiddleware({
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(is|en)/:path*'],
+  matcher: ['/', '/(en|is)/:path*'],
 };
