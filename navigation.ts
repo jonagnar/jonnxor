7f23931daff15f6b80332833b9808725591f82b2
JonnXor <jonnxor@gmail.com>
2024-06-13 13:37:40 +0000

    feat: remove locale from url

--- navigation.ts
+++ navigation.ts
@@ -2,7 +2,7 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 export const defaultLocale = 'is' as const;
 export const locales = ['is', 'en'] as const;
-export const localePrefix = 'always'; // Default
+export const localePrefix = 'never'; // Default
 
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({
   locales,
