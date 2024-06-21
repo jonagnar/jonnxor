6b8692f081f0e456a56d67f592649b23494091f9
JonnXor <jonnxor@gmail.com>
2024-06-21 11:59:05 +0000

    feat: fix webpage.

--- i18n.ts
+++ i18n.ts
@@ -2,10 +2,10 @@ import { cookies } from 'next/headers';
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
-import { locales } from '@/config';
+import { defaultLocale, locales } from '@/config';
 
-export default getRequestConfig(async () => {
-  const locale = cookies().get('NEXT_LOCALE')?.value;
+export default getRequestConfig(async (params) => {
+  const locale = cookies().get('NEXT_LOCALE')?.value || params.locale || defaultLocale;
 
   //* Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
