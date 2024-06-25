0f2dd82c740cfcfed18d860a36803c6f28e821f1
JonnXor <jonnxor@gmail.com>
2024-06-25 21:36:18 +0000

    feat: remove alfreð

--- features/social/social.tsx
+++ features/social/social.tsx
@@ -1,40 +1,45 @@
 'use client';
 
 import clsx from 'clsx';
-import { type GroupProps, Group, ActionIcon, Button } from '@mantine/core';
+import { type GroupProps, Group, ActionIcon } from '@mantine/core';
 import { IconBrandLinkedin, IconBrandGithub, IconShare } from '@tabler/icons-react';
 
-import { Image } from '@/components/image/image';
-
-import alfred from '@/public/alfred.png';
 import classes from './social.module.css';
 
 export type SocialProps = GroupProps & {};
 
 export function Social({ className, ...props }: SocialProps) {
+  const share = () => null;
+
   return (
-    <Group my="xl" wrap="nowrap" {...props} className={clsx(classes.social, className)}>
-      <Group my="xl" justify="space-between" style={{ flex: 1 }}>
-        <Button size="xl" py="xs" px="xl" variant="subtle">
-          <Image src={alfred} alt="Alfreð" style={{ flex: 1, maxHeight: '100%' }} />
-        </Button>
-        <Group ml="xl">
-          <ActionIcon
-            component="a"
-            href="https://www.linkedin.com/in/jonagnar/"
-            size="xl"
-            radius="xl"
-          >
-            <IconBrandLinkedin />
-          </ActionIcon>
-          <ActionIcon component="a" href="https://github.com/jonagnar" size="xl" radius="xl">
-            <IconBrandGithub />
-          </ActionIcon>
-          <ActionIcon component="a" href="#" size="xl" radius="xl">
-            <IconShare />
-          </ActionIcon>
-        </Group>
-      </Group>
+    <Group
+      my="xl"
+      wrap="nowrap"
+      justify="space-between"
+      {...props}
+      className={clsx(classes.social, className)}
+    >
+      <ActionIcon
+        component="a"
+        href="https://www.linkedin.com/in/jonagnar/"
+        target="_blank"
+        size="xl"
+        radius="xl"
+      >
+        <IconBrandLinkedin />
+      </ActionIcon>
+      <ActionIcon
+        component="a"
+        href="https://github.com/jonagnar"
+        target="_blank"
+        size="xl"
+        radius="xl"
+      >
+        <IconBrandGithub />
+      </ActionIcon>
+      <ActionIcon size="xl" radius="xl" onClick={share}>
+        <IconShare />
+      </ActionIcon>
     </Group>
   );
 }
