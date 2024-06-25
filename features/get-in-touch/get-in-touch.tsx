6a1fe792c567e50b23a9eb7e9c5ccbece490cf58
JonnXor <jonnxor@gmail.com>
2024-06-25 17:35:48 +0000

    feat: add spacing to profile comp.

--- features/get-in-touch/get-in-touch.tsx
+++ features/get-in-touch/get-in-touch.tsx
@@ -4,12 +4,16 @@ import { Title, Text } from '@mantine/core';
 
 import { ContactIconsList } from '@/features/contact-icons/contact-icons';
 
+import classes from './get-in-touch.module.css';
+
 export function GetInTouch() {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <Title order={3}>Jón Agnar Stefánsson</Title>
       <Text>Web Developer</Text>
-      <ContactIconsList />
+      <div className={classes.contact}>
+        <ContactIconsList />
+      </div>
     </div>
   );
 }
