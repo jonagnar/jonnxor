18f007801c671db7956a535ad8636c0d319be580
JonnXor <jonnxor@gmail.com>
2024-06-25 17:29:36 +0000

    fix: text was wrong, my bad.

--- features/summary/summary.tsx
+++ features/summary/summary.tsx
@@ -7,7 +7,7 @@ import classes from './summary.module.css';
 export function Summary() {
   return (
     <div className={classes.wrapper}>
-      <Title order={3}>Experience</Title>
+      <Title order={3}>Summary</Title>
       <Text>
         Experienced computer scientist specialized in frontend web development. Technology Stack:
         TypeScript, React, Next.js, and more with over eight years of industry experience
