036403e3f123b924f98b1e33915a4a2d4917412e
JonnXor <jonnxor@gmail.com>
2024-07-03 15:00:19 +0000

    feat: update metadatabase property.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -43,6 +43,7 @@ export async function generateMetadata({ params: { locale } }: Params) {
   return {
     title: t('metadata.title'),
     description: t('metadata.description'),
+    metadataBase: new URL('https://jonnxor.is'),
   };
 }
 
